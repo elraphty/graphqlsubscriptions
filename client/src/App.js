ba08c395daf9ce23f3bb237c810ab49411cb641e
elraphty <elraphtyofficial@gmail.com>
2019-08-11 16:10:31 +0000

    after setting up apollo-client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ApolloClient } from 'apollo-client';
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { getMainDefinition } from 'apollo-utilities';
+import { ApolloProvider } from 'react-apollo';
 import './App.css';
+
+// Create an http link:
+const httpLink = new HttpLink({
+  uri: 'http://localhost:7000/graphql'
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:7000/subscriptions`,
+  options: {
+    reconnect: true
+  }
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+
+const cache = new InMemoryCache();
+const client = new ApolloClient({ link, cache });
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+      </div>
+    </ApolloProvider>
   );
 }
 
